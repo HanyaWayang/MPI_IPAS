4ce5ebead87e4efcef026e15e4a77794e5d83522
HanyaWayang <miftahul912@guru.sd.belajar.id>
2024-09-26 12:58:12 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,128 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5zFM3Hb4KeP":
+        Script1();
+        break;
+      case "6puCt1jutop":
+        Script2();
+        break;
+      case "6dqse0Btdot":
+        Script3();
+        break;
+      case "5cAapXSYOww":
+        Script4();
+        break;
+      case "6Tuypo3QRMF":
+        Script5();
+        break;
+      case "5t3Vd2pjtRt":
+        Script6();
+        break;
+      case "6rEA6POkdNC":
+        Script7();
+        break;
+      case "5k88ldKVyRe":
+        Script8();
+        break;
+      case "5emBZb8QotC":
+        Script9();
+        break;
+      case "5gvyu37WPOv":
+        Script10();
+        break;
+      case "5tZP7DTbqjP":
+        Script11();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script3()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script4()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script5()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script6()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script7()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script8()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script9()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script10()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
+function Script11()
+{
+  var audio = document.getElementById(‘bgSong’);
+audio.src=”musik.mp3”;
+audio.loud();
+audio.play();
+}
+
